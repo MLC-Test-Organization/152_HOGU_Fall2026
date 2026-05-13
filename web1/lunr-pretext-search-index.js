32184ac72a6bae0e0178e78a58b53aebbeae19be
Christy Nakad <christynakad@tamu.edu>
2026-05-13 14:11:50 +0000

    Latest build deployed.

---
+++ web1/lunr-pretext-search-index.js
@@ -0,0 +1,122 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "4827",
+  "level": "1",
+  "url": "#4827",
+  "type": "Article",
+  "number": "",
+  "title": "MATH # Hands-On, Grades Up",
+  "body": " MATH # Hands-On, Grades Up   For workspace between exercises, print this worksheet or download to your device by clicking the printer icon.  The Math 142 textbook is free and can be downloaded as a PDF at Texas A&M The OAKTrust Digital Repository .    Session #2  Directions: Answer each of the following as best you can. Feel free to discuss your thoughts about the questions with some peers around you.    Getting Warmed Up and in the Right Mindset!!    Given and , what do you then know about any horizontal asymptotes of ?      Given the following information about , what do you know about the horizontal and vertical asymptotes?       Determine each of the following limits.                 Examine the limits above, group them together by finding patterns or common characteristics, then fill in the blanks below.    Given where is any real number and , then and determine .    Given where , then and determine .    Given where , then and determine .         Let Us Get Our Hands On    Find any (a) horizontal asymptotes, (b) the coordinates of any holes, and (c) vertical asymptotes of the function. If there are no horizontal asymptotes, describe the end behavior using limit notation.                           Given , follow the steps below to find the horizontal asymptotes of .     Set up but do not yet solve the two limits that are needed to determine the horizontal asymptotes.      Fill in the blanks with either positive or negative .    If , we divide all terms by the in the denominator that has the most ______ power.    If , we divide all terms by the in the denominator that has the most ______ power.        Apply the techniques from part (b) to compute the limits in part (a) and state the horizontal asymptotes of .       A function is continuous at a point where if and only if the following three conditions are satisfied:   ______ is defined.  exists.  ______.       If you know and , can you correctly conclude that is continuous at ? Why or why not?       Now Grades Go Up    The graph of is not continuous at because:   is not defined.  does not exist.  .    I and III only  III only  II and III only  I, II, and III  II only       Which of the following is true regarding the horizontal asymptotes for ?   does not have any horizontal asymptotes.    has two horizontal asymptotes; one at and another at .     has only one horizontal asymptote at .     has only one horizontal asymptote at .     has two horizontal asymptotes; one at and another at .        Suppose for some function , you know . What can you conclude happens at ?   has a vertical asymptote at .  has a horizontal asymptote at .  .  has a hole at .  .       "
+},
+{
+  "id": "4827-3-3-2",
+  "level": "2",
+  "url": "#4827-3-3-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Given and , what do you then know about any horizontal asymptotes of ?   "
+},
+{
+  "id": "4827-3-3-3",
+  "level": "2",
+  "url": "#4827-3-3-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Given the following information about , what do you know about the horizontal and vertical asymptotes?    "
+},
+{
+  "id": "4827-3-3-4",
+  "level": "2",
+  "url": "#4827-3-3-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Determine each of the following limits.              "
+},
+{
+  "id": "4827-3-3-5",
+  "level": "2",
+  "url": "#4827-3-3-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  Examine the limits above, group them together by finding patterns or common characteristics, then fill in the blanks below.    Given where is any real number and , then and determine .    Given where , then and determine .    Given where , then and determine .     "
+},
+{
+  "id": "4827-3-4-2",
+  "level": "2",
+  "url": "#4827-3-4-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Find any (a) horizontal asymptotes, (b) the coordinates of any holes, and (c) vertical asymptotes of the function. If there are no horizontal asymptotes, describe the end behavior using limit notation.                        "
+},
+{
+  "id": "4827-3-4-3",
+  "level": "2",
+  "url": "#4827-3-4-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Given , follow the steps below to find the horizontal asymptotes of .     Set up but do not yet solve the two limits that are needed to determine the horizontal asymptotes.      Fill in the blanks with either positive or negative .    If , we divide all terms by the in the denominator that has the most ______ power.    If , we divide all terms by the in the denominator that has the most ______ power.        Apply the techniques from part (b) to compute the limits in part (a) and state the horizontal asymptotes of .    "
+},
+{
+  "id": "4827-3-4-4",
+  "level": "2",
+  "url": "#4827-3-4-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  A function is continuous at a point where if and only if the following three conditions are satisfied:   ______ is defined.  exists.  ______.    "
+},
+{
+  "id": "4827-3-4-5",
+  "level": "2",
+  "url": "#4827-3-4-5",
+  "type": "Worksheet Exercise",
+  "number": "4",
+  "title": "",
+  "body": "  If you know and , can you correctly conclude that is continuous at ? Why or why not?   "
+},
+{
+  "id": "4827-3-5-2",
+  "level": "2",
+  "url": "#4827-3-5-2",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  The graph of is not continuous at because:   is not defined.  does not exist.  .    I and III only  III only  II and III only  I, II, and III  II only    "
+},
+{
+  "id": "4827-3-5-3",
+  "level": "2",
+  "url": "#4827-3-5-3",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Which of the following is true regarding the horizontal asymptotes for ?   does not have any horizontal asymptotes.    has two horizontal asymptotes; one at and another at .     has only one horizontal asymptote at .     has only one horizontal asymptote at .     has two horizontal asymptotes; one at and another at .     "
+},
+{
+  "id": "4827-3-5-4",
+  "level": "2",
+  "url": "#4827-3-5-4",
+  "type": "Worksheet Exercise",
+  "number": "3",
+  "title": "",
+  "body": "  Suppose for some function , you know . What can you conclude happens at ?   has a vertical asymptote at .  has a horizontal asymptote at .  .  has a hole at .  .    "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
